81bf784b3738f5ecf54327135d6f89b082344d1c
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-05 23:50:59 +0100

    Controller getRobotById added

--- server/controller/robotController.js
+++ server/controller/robotController.js
@@ -4,3 +4,20 @@ const getRobot = async (req, res) => {
   const robots = await Robot.find();
   res.json(robots);
 };
+
+const getRobotById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const searchedRobot = await Robot.findById(id);
+    if (searchedRobot) {
+      res.json(searchedRobot);
+    } else {
+      const error = new Error("Robot not found");
+      error.code = 404;
+      throw error;
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
+};
