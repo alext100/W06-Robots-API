d5495a8713f73495315b7fb83423b79b499cb428
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-09 13:42:22 +0100

    To userRoutes validate(loginRequestSchema) was added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { validate } = require("express-validation");
 const loginUser = require("../controller/userController");
+const { loginRequestSchema } = require("../../database/models/loginUserSchema");
 
 const router = express.Router();
 
-router.post("/login", loginUser);
+router.post("/login", validate(loginRequestSchema), loginUser);
 
 module.exports = router;
